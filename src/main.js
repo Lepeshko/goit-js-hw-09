1717e33af36ebf1aca11b717a5c96ca621486ef5
Lepeshko <anatolii.lepeshko1@gmail.com>
2024-05-22 15:10:18 +0300

    commit-22.05.24-15.10

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { taskGallery } from "./js/1-gallery";
+import { taskForm } from "./js/2-form";
+
+
+console.log("Horray my boundler works");
+console.log(taskGallery);
+console.log(taskForm);
